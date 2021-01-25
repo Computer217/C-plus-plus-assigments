364b7ea8ba5bd9a0ae23915a6dce3763811f640f
Pablo Rodriguez <pablo.rodriguez@uconn.edu>
2021-01-24 19:17:04 -0500

    Lab 01/25/21

---
+++ RemoveSpaces/RemoveSpaces.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string RemoveSpaces( const string &strIn)
+{
+    // remove all spaces in strIn and return this new string
+    // The function _RemoveSpaces_ cannot alter the strIn variable values of the calling function
+    //since you cant ALTER <strIn> then you can only USE <strIn> as shown below...
+    // your code goes here...
+    string temporary;
+    int len = strIn.size();
+
+    for (int i = 0; i < len; i++){
+	    if (strIn[i] != ' '){
+		    temporary = temporary + strIn[i];
+	    }
+    }
+
+
+
+
+    return temporary;
+}
+
+// now test it out
+int main()
+{
+    const string str1 = "I am a student.";
+    const string str2 = "";
+    const string str3 = "This   is   wrong!";
+    string str1Out = RemoveSpaces(str1);
+    cout << str1Out << endl;
+    string str2Out = RemoveSpaces(str2);
+    cout << str2Out << endl;
+    string str3Out = RemoveSpaces(str3);
+    cout << str3Out << endl;
+    return 0;
+}
+
